e6b8cdcc94406b1bf3779910693ae8f656bdf22e
Danny Chiou <dany.chiou@gmail.com>
2026-06-07 01:59:58 +0200

    erstelle Karten-Element für jedes Tier

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -14,58 +14,47 @@ def load_html_template(file_path):
 
 
 def generate_animals_info(animals_data):
-    """Creates a string containing all animal information."""
+    """Creates HTML list items for all animals."""
     output = ""
 
     for animal in animals_data:
+        output += '<li class="cards__item">\n'
+
         if "name" in animal:
-            output += f"Name: {animal['name']}<br>\n"
+            output += f"Name: {animal['name']}<br/>\n"
 
         if (
             "characteristics" in animal
             and "diet" in animal["characteristics"]
         ):
-            output += (
-                f"Diet: {animal['characteristics']['diet']}<br>\n"
-            )
+            output += f"Diet: {animal['characteristics']['diet']}<br/>\n"
 
         if "locations" in animal and len(animal["locations"]) > 0:
-            output += f"Location: {animal['locations'][0]}<br>\n"
+            output += f"Location: {animal['locations'][0]}<br/>\n"
 
         if (
             "characteristics" in animal
             and "type" in animal["characteristics"]
         ):
-            output += (
-                f"Type: {animal['characteristics']['type']}<br>\n"
-            )
+            output += f"Type: {animal['characteristics']['type']}<br/>\n"
 
-        output += "<br>\n"
+        output += "</li>\n"
 
     return output
 
 
 def main():
     animals_data = load_data("animals_data.json")
+    html_template = load_html_template("animals_template.html")
 
-    html_template = load_html_template(
-        "animals_template.html"
-    )
-
-    animals_info = generate_animals_info(
-        animals_data
-    )
+    animals_info = generate_animals_info(animals_data)
 
     final_html = html_template.replace(
         "__REPLACE_ANIMALS_INFO__",
         animals_info
     )
 
-    with open(
-        "animals.html",
-        "w",
-        encoding="utf-8"
-    ) as file:
+    with open("animals.html", "w", encoding="utf-8") as file:
         file.write(final_html)
 
     print("animals.html generated successfully!")
